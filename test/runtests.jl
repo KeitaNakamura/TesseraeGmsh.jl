79b2e54ad2dc805725336b53bb67d124202c836a
Keita Nakamura <keita.nakamura.1109@gmail.com>
2025-07-31 16:22:48 +0900

    Drop support for Tesserae versions 0.2 and 0.3

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,13 +7,13 @@ using Test
     # body
     mesh_body = meshes["body"]
     particles = generate_particles(@NamedTuple{x::Vec{3,Float64}, detJdΩ::Float64}, mesh_body)
-    mpvalues = generate_mpvalues(mesh_body, size(particles))
-    feupdate!(mpvalues, mesh_body; volume = particles.detJdΩ)
+    weights = generate_interpolation_weights(mesh_body, size(particles))
+    feupdate!(weights, mesh_body; volume = particles.detJdΩ)
     @test sum(particles.detJdΩ) ≈ 4π/3 rtol=0.01
     # surface
     mesh_surface = meshes["surface"]
     particles = generate_particles(@NamedTuple{x::Vec{3,Float64}, detJdΩ::Float64}, mesh_surface)
-    mpvalues = generate_mpvalues(mesh_surface, size(particles))
-    feupdate!(mpvalues, mesh_surface; area = particles.detJdΩ)
+    weights = generate_interpolation_weights(mesh_surface, size(particles))
+    feupdate!(weights, mesh_surface; area = particles.detJdΩ)
     @test sum(particles.detJdΩ) ≈ 4π rtol=0.001
 end
